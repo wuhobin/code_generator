278fcb243437b7267ec3a2e75b4633f76e24ab5b
wuhobin <wuhongbin@365huaer.net>
2024-03-20 20:40:44 +0800

    搭建项目

---
+++ aurora-chat/src/main/java/com/chat/aurorachat/AuroraChatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.aurorachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuroraChatApplication.class, args);
+    }
+
+}
