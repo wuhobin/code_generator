4c5b87ee1a6039e76224a6b6420150b6ee7d62e9
wuhobin <wuhongbin@365huaer.net>
2024-03-22 18:33:02 +0800

    添加sa-token框架

---
+++ aurora-chat/src/main/java/com/chat/aurorachat/config/SaTokenConfigure.java
@@ -0,0 +1,23 @@
+package com.chat.aurorachat.config;
+
+import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.stp.StpUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Sa-Token 配置
+ * @author admin
+ */
+@Configuration
+public class SaTokenConfigure implements WebMvcConfigurer {
+    // 注册拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册 Sa-Token 拦截器，校验规则为 StpUtil.checkLogin() 登录校验。
+        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/doLogin"); 
+    }
+}
